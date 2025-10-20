c290ce0f30f55707190e84d1baa5440f0986493b
gabivitoria2 <gabriellivitoria321@gmail.com>
2025-10-20 09:36:19 -0300

    up

---
+++ exe1.js
@@ -0,0 +1,6 @@
+console.log("Hello, Word!");
+console.log('soma:',3+2);
+console.leg('subtração:',3-2);
+console.log('divisão:',3/2);
+console.log('multiplicação:'3*2);
+console.log('resro da divisão:',3%2):
